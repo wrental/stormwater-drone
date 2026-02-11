a33c0442673b3dd7d3cdf6dfccc3fe2c27b94f8f
wrental <wren.sobo@pm.me>
2026-02-10 19:26:38 -0500

    spi bus testing (incomplete)

--- main/v2-stormwater-drone.c
+++ main/v2-stormwater-drone.c
@@ -1,34 +1,19 @@
-// name: v2-stormwater-drone.c
-// date: 02-10-2026
-// auth: wrental
-// desc: version way more than 2 ngl
+/* name: v2-stormwater-drone.c
+ * date: 02-10-2026
+ * auth: wrental
+ * desc: version way more than 2 ngl
+ */
 
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "driver/gpio.h"
 #include "esp_lora_1121.h"
-#include "driver/spi_common.h"
-#include "driver/spi_master.h"
-
-
-// needed functions:
-// - init()
-// - getTemp()
-// - getDO2()
-// - getPH()
-
-// LoRa setup:
 
 void app_main(void)
 {
 	// program setup
-	int x = 0;
+	char* version = lr11xx_driver_version_get_version_string();
+	printf("lr11xx driver version: %s\n", version);
 
 	// program loop
-	while(1) {
-		x = x + 1;
-		printf("loop: %d\n", x);
-	}
 }
 
 
