b331b607be9a7fd5b235da66049af9893ed1ea25
wrental <wren.sobo@pm.me>
2026-02-11 23:06:42 -0500

    spi success

--- main/v3_stormwater_drone.c
+++ main/v3_stormwater_drone.c
@@ -6,26 +6,34 @@
 
 
 #include "v3_stormwater_drone.h"
-#include "lr1121_config.h"
 
+// helper functions
+
+static bool irq_flag;
+static void IRAM_ATTR isr(void* arg) {
+  irq_flag = true; // Set the interrupt flag
+}
+
+
+// --- MAIN ---
 void app_main(void) {
 
-	// ESP-32-S3 SPI Initialization
-	printf("Initializing SPI2_HOST\n");
-	spi_bus_initialize(SPI2_HOST, &spi_bus_config, SPI_DMA_CH_AUTO);
-	spi_bus_add_device(SPI2_HOST, &spi_device_interface_config, &spi_device_handle);
+  // ESP-32-S3 SPI Initialization
 
-	// Waveshare Core1121 HF Initialization
-	printf("Initializing LoRa over SPI...\n");
-	lora_init_io_context(&lr1121, ESP_SPI_CS, ESP_SPI_RESET, ESP_SPI_BUSY, ESP_SPI_INT);
-	lora_init_io(&lr1121);
-	lora_spi_init(&lr1121, spi_device_handle);
-	lora_system_init(&lr1121);
-	lora_print_version(&lr1121);
-	lora_radio_init(&lr1121);
-	lora_init_irq(&lr1121, isr);
+  printf("Initializing SPI2_HOST\n");
+  spi_bus_initialize(SPI2_HOST, &spi_bus_config, SPI_DMA_CH_AUTO);
+  spi_bus_add_device(SPI2_HOST, &spi_device_interface_config, &spi_device_handle);
 
 
+  // Waveshare Core1121 HF Initialization
 
-	
+  printf("Initializing LoRa over SPI...\n");
+  lora_init_io_context(&lr1121, ESP_SPI_CS, ESP_SPI_RESET, ESP_SPI_BUSY, ESP_SPI_INT);
+  lora_init_io(&lr1121);
+  lora_spi_init(&lr1121, spi_device_handle);
+  lora_system_init(&lr1121);
+  lora_print_version(&lr1121);
+  lora_radio_init(&lr1121);
+  lora_init_irq(&lr1121, isr);
+  
 }
--- main/v3_stormwater_drone.h
+++ main/v3_stormwater_drone.h
@@ -5,10 +5,11 @@
 #include <stdio.h>
 
 // esp-idf common components
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "driver/spi_common.h"
 #include "driver/spi_master.h"
 #include "hal/spi_types.h"
-#include "esp_err.h"
 
 // waveshare/esp_lora_1121
 #include "esp_lora_1121.h"
@@ -22,7 +23,7 @@
 #define ESP_SPI_RESET           (GPIO_NUM_5)
 #define ESP_SPI_BUSY            (GPIO_NUM_6)
 
-#define ESP_SPI_INT             (GPIO_NUM_7)    // interrupt pin >> 1121_DIO9
+#define ESP_SPI_INT             (GPIO_NUM_7)    // interrupt pin >> DIO9
 
 #define ESP_SPI_NUM             (SPI2_HOST)
 #define ESP_SPI_CLK_SPEED_HZ    8 * 1000 * 1000 // 8MHz
@@ -32,26 +33,21 @@
       LR11XX_SYSTEM_IRQ_HEADER_ERROR | LR11XX_SYSTEM_IRQ_CRC_ERROR | LR11XX_SYSTEM_IRQ_FSK_LEN_ERROR )
 
 static spi_bus_config_t spi_bus_config = {
-	.sclk_io_num = ESP_SPI_CLK,
-	.mosi_io_num = ESP_SPI_MOSI,
-	.miso_io_num = ESP_SPI_MISO,
+  .sclk_io_num = ESP_SPI_CLK,
+  .mosi_io_num = ESP_SPI_MOSI,
+  .miso_io_num = ESP_SPI_MISO,
   .quadwp_io_num = -1,
   .quadhd_io_num = -1,
   .max_transfer_sz = 64,
 };
 
 static spi_device_interface_config_t spi_device_interface_config = {
-	.clock_speed_hz = ESP_SPI_CLK_SPEED_HZ,		// 8 MHz
+  .clock_speed_hz = ESP_SPI_CLK_SPEED_HZ,		// 8 MHz
   .mode = 0,	// SPI mode 0: CPOL=0, CPHA=0
   .spics_io_num = -1,
   .queue_size = 1,
 };
 
 static spi_device_handle_t spi_device_handle = NULL; 	// SPI handle
-
-static bool irq_flag;
-static void IRAM_ATTR isr(void* arg) {
-    irq_flag = true; // Set the interrupt flag
-}
 
 #endif
