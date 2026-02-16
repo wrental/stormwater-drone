e5173f6139b50a54b8e52fa09a5d457be39bcb6a
wrental <wren.sobo@pm.me>
2026-02-16 15:53:04 -0500

    feat: lora/spi setup via helper functions

--- main/stormwater_drone.c
+++ main/stormwater_drone.c
@@ -6,27 +6,20 @@
 
 
 #include "stormwater_drone.h"
+#include "lr1121_config.h"
+#include "lr11xx_types.h"
+#include "lr11xx_types_str.h"
 
 // helper functions
-
 static bool irq_flag;
 static void IRAM_ATTR isr(void* arg) {
   irq_flag = true; // Set the interrupt flag
 }
 
-
-// --- MAIN ---
-void app_main(void) {
-
-  // ESP-32-S3 SPI Initialization
-
-  printf("Initializing SPI2_HOST\n");
-  spi_bus_initialize(SPI2_HOST, &spi_bus_config, SPI_DMA_CH_AUTO);
-  spi_bus_add_device(SPI2_HOST, &spi_device_interface_config, &spi_device_handle);
-
-
-  // Waveshare Core1121 HF Initialization
-
+/**
+ *  @brief lora init via lora_config.h, esp_lora_1121.h functions 
+ */
+static void init_lora(void) {
   printf("Initializing LoRa over SPI...\n");
   lora_init_io_context(&lr1121, ESP_SPI_CS, ESP_SPI_RESET, ESP_SPI_BUSY, ESP_SPI_INT);
   lora_init_io(&lr1121);
@@ -35,5 +28,29 @@ void app_main(void) {
   lora_print_version(&lr1121);
   lora_radio_init(&lr1121);
   lora_init_irq(&lr1121, isr);
-  
+  lr11xx_status_t radio_status = lr11xx_radio_set_rx(&lr1121, RX_CONTINUOUS);
+  printf("%s\n", lr11xx_status_to_str(radio_status));
+}
+
+/*
+ * @brief spi init via driver/spi_*.h components from esp-idf
+ */
+static void init_spi(void) {
+  printf("Initializing SPI2_HOST\n");
+  spi_bus_initialize(SPI2_HOST, &spi_bus_config, SPI_DMA_CH_AUTO);
+  spi_bus_add_device(SPI2_HOST, &spi_device_interface_config, &spi_device_handle);
+}
+
+
+
+/*
+ * @brief main app - call init functions, start loop
+ */
+void app_main(void) {
+  init_spi();
+  init_lora();
+
+
+
+    
 }
